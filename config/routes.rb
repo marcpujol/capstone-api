878500191f175032525ed7bd27a9019f6f9a3fbc
Marc Pujol <marc.pujol@xing.com>
2017-02-14 12:50:13 +0100

    Add module 2 practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope :api, defaults: {format: :json} do
-    resources :states, only: [:index, :show]#except: [:new, :edit]
-    resources :cities, only: [:index, :show]#except: [:new, :edit]
+    resources :states #, only: [:index, :show] #except: [:new, :edit]
+    resources :cities #, only: [:index, :show] #except: [:new, :edit]
   end
 
   get '/ui' => 'ui#index'
